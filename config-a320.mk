5e508f5a3346ab32b5f36b0ff5c2309f0c1a3b9c
blackz1982 <blackz1982@gmail.com>
2018-04-07 19:49:11 +0300

    Update config-a320.mk

--- config-a320.mk
+++ config-a320.mk
@@ -9,8 +9,6 @@ BACKEND_KILL := Y
 # Disabled for now; doesn't work well with double buffering
 # BACKEND_SCREENSHOT := Y
 
-DEFAULT_MIXER = "\"SoftMaster\""
-
 CROSS_COMPILE?=mipsel-a320-linux-uclibc-
 
 CFLAGS += -DWITH_REVERSED_X_Y
